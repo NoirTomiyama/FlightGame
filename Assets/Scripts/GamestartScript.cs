849c32ac7a0a2ae4f4e56e5ba7bf34790d5ba32b
Ryota Tomiyama <lit.karu.noir@gmail.com>
2019-09-23 14:05:27 +0900

    スクリプト名変更

--- Assets/Scripts/GamestartScript.cs
+++ Assets/Scripts/GamestartScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GamestartScript : MonoBehaviour
+public class GameStartScript : MonoBehaviour
 {
   // Start is called before the first frame update
   void Start()
